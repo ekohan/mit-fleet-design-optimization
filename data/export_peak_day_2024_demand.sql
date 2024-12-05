2521382b814c9c82db4f7164969cfb67ec729262
fabricioat <fabriciosk8@gmail.com>
2024-12-04 21:21:58 -0300

    New peak day for 2024 data.

--- data/export_peak_day_2024_demand.sql
+++ data/export_peak_day_2024_demand.sql
@@ -9,6 +9,6 @@ FROM sales_2024
 WHERE Lat BETWEEN 4.3333 AND 4.9167 
     AND Lon BETWEEN -74.3500 AND -73.9167  -- Bogota Metro Area bounding box
     AND Kg >= 0  -- Has demand
-    AND Date = '2024-08-01'  -- Filter for specific day
+    AND Date = '2024-07-18'  -- Filter for specific day
 GROUP BY Date, ClientID, ProductType, Lat, Lon
 
