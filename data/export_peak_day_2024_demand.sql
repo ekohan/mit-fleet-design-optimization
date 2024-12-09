6e1543835bbe41c9cda0141861c5ea1112cef64d
fabricioat <fabriciosk8@gmail.com>
2024-12-09 18:56:52 -0300

    New filters and demand days.

--- data/export_peak_day_2024_demand.sql
+++ data/export_peak_day_2024_demand.sql
@@ -8,8 +8,8 @@ SELECT
 FROM sales
 WHERE Lat BETWEEN 4.3333 AND 4.9167 
     AND Lon BETWEEN -74.3500 AND -73.9167  -- Bogota Metro Area bounding box
-    AND Date = '2024-08-05'  -- Filter for specific day
+    AND Date = '2024-06-05'  -- Filter for specific day
 GROUP BY Date, ClientID, ProductType, Lat, Lon
-HAVING SUM(Kg) >= 10  -- Remove small demand at client level
+HAVING SUM(Kg) >= 40  -- Remove small demand at client level
     AND SUM(Kg) <= 4000  -- No outliers at client level
 
