2aeec80e51e7ab1883bd81a5d7c9a6b6357333c4
Eric Kohan <ekohan@gmail.com>
2025-05-11 18:13:58 -0300

    refa to instrument timing of each phase

--- src/benchmarking/benchmark_types.py
+++
@@ -1,8 +0,0 @@
-from src.core_types import BenchmarkType # noqa: F401
-
-# BenchmarkType enum is now defined in src.core_types.py
-
-class BenchmarkType(Enum):
-    """Types of VRP benchmarks."""
-    SINGLE_COMPARTMENT = "single_compartment"  # Upper bound - Separate VRPs per product
-    MULTI_COMPARTMENT = "multi_compartment"    # Lower bound - Aggregate demand, post-process for compartments
